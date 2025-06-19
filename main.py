8b7ce5a138b0d0e31b42dcee31d21ce873b8840e
Ace Perez <aceperez@Aces-MacBook-Pro.local>
2025-06-19 13:22:47 +0100

    added context to code

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 
+# Initialize Flask application
 app = Flask(__name__)
 
+# Route definitions
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -18,5 +20,6 @@ def education():
 def projects():
     return render_template("projects.html")
 
+# Run the application, used port 8000 because for some reason something is occupying my port 5000 lol
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
